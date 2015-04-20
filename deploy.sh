19f2f8b641981251a8501c6c57bcf15f4745750c
Igor Minar <igor@angularjs.org>
2015-04-20 03:44:24 -0700

    chore(deploy): keep node_modules in place after deploy

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ git checkout -b deploy
 git add node_modules/angular/
 git commit -m "deploy build `date`"
 git push -f upstream deploy:gh-pages
+git reset HEAD~1
 git checkout -
 git branch -d deploy
